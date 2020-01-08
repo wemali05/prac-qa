476f77649f35747e7d5e3adcca1fdc3354135d9d
wemali05 <alfredwemali05@gmail.com>
2020-01-09 00:23:19 +0300

    alert messages

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -20,7 +20,7 @@
                         @csrf
                         <div class="form-group">
                             <label for="question-title">Title</label>
-                            <input type="text" name="title" id="question-title"
+                            <input type="text" name="title" id="question-title" value="{{ old('title')}}"
                                 class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
 
                             @if($errors->has('title'))
@@ -32,7 +32,7 @@
                         <div class="form-group">
                             <label for="question-body">Explain you question</label>
                             <textarea name="body" id="question-body" rows="10"
-                                class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></textarea>
+                                class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}">{{ old('body')}}</textarea>
 
                             @if($errors->has('body'))
                             <div class="invalid-feedback">
