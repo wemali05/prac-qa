a71450e77a8209582ca98b22f703f7ee9e1326a0
wemali05 <alfredwemali05@gmail.com>
2020-01-09 20:23:59 +0300

    edit question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -16,10 +16,10 @@
                 </div>
 
                 <div class="card-body">
-
+                    @include('layouts._messages')
 
                     @foreach($questions as $question)
-                    @include('layouts._messages')
+
                     <div class="media">
                         <div class="d-flex flex-column counters">
                             <div class="vote">
@@ -34,7 +34,14 @@
                             </div>
                         </div>
                         <div class="media-body">
-                            <h3 class="mt-0"><a href="{{ $question->url}}"> {{ $question->title}}</a></h3>
+                            <div class="d-flex align-items-center">
+                                <h3 class="mt-0"><a href="{{ $question->url}}"> {{ $question->title}}</a></h3>
+                                <div class="ml-auto">
+                                    <a href="{{ route('questions.edit', $question->id)}}"
+                                        class="btn btn-sm btn-outline-info">edit</a>
+                                </div>
+                            </div>
+
                             <p class="lead">
                                 Asked by
                                 <a href="{{ $question->user->url}}"> {{ $question->user->name }}</a>
